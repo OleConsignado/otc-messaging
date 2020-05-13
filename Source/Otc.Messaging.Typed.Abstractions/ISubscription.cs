0efadbd0c04b303285b2324b440a7744d2e70953
Alexandre Brina <alexbrina@gmail.com>
2020-05-13 09:56:36 -0300

    Simplified publishing of typed messages

--- Source/Otc.Messaging.Typed.Abstractions/ISubscription.cs
+++ Source/Otc.Messaging.Typed.Abstractions/ISubscription.cs
@@ -3,6 +3,7 @@
 namespace Otc.Messaging.Typed.Abstractions
 {
     /// <inheritdoc cref="ISubscription"/>
+    /// <typeparam name="T">The message object type</typeparam>
     public interface ISubscription<out T> : ISubscription
     {
     }
---
+++ Source/Otc.Messaging.Typed/PublisherExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Otc.Messaging.Abstractions;
+
+namespace Otc.Messaging.Typed
+{
+    public static class PublisherExtensions
+    {
+        /// <inheritdoc cref="IPublisher"/>
+        /// <typeparam name="T">The message object type</typeparam>
+        public static void Publish<T>(this IPublisher publisher, T message, string topic, string queue = null, string messageId = null)
+        {
+            var messageBytes = MessagingExtensions.Serializer.Serialize(message);
+            publisher.Publish(messageBytes, topic, queue, messageId);
+        }
+    }
+}
